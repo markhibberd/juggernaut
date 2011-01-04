a5fbeede9a6e30e55c7485e10a5259defa7c779a
Mark Hibberd <mark@hibberd.id.au>
2011-01-04 22:56:36 +1000

    initial splatterings

--- src/Juggernaut/Api.hs
+++ src/Juggernaut/Api.hs
@@ -5,17 +5,25 @@ import Control.Monad
 type Dependency = (String, [String])
 type Dependencies = [Dependency]
 
+
+
 pull :: Dependency -> String
 pull (d, _) = d ++ "-pull:\n\tFIX: fill this in automatically\n\n"
 
 pulls :: Dependencies -> [String]
 pulls = map pull
 
+
+
+
 up :: Dependency -> String
-up (d, ds) = d ++ "-up: " ++ (join (map (\x -> x ++ "-up") ds)) ++ "\n\t${MAKE} " ++ d ++ "\n\n"
+up (d, ds) = d ++ "-up: " ++ (join (map (\x -> x ++ "-up ") ds)) ++ "\n\t${MAKE} " ++ d ++ "\n\n"
 
 ups :: Dependencies -> [String]
 ups = map up
+
+
+
 
 isDownstream :: String -> Dependency -> Bool
 isDownstream s (_, ds) = elem s ds
@@ -29,12 +37,18 @@ down (d, _) dn = d ++ "-down: " ++ d ++ "\n\t${MAKE} " ++ (join (map (\x -> x ++
 downs :: Dependencies -> [String]
 downs d = map (\x -> down x (downstream (fst x) d)) d
 
+
+
+
 self :: (String -> String) -> Dependency -> String
 self f (d, _) = d ++ ": " ++ d ++ "-pull\n\t" ++ (f d) ++ "\n\n"
 
 selfs :: Dependencies -> (String -> String) -> [String]
 selfs ds f = map (self f) ds
           
+
+
+
 genmakefile :: Dependencies -> (String -> String) -> String
 genmakefile ds f = join (genmakefile' ds f)
 
--- src/Juggernaut/Cli.hs
+++ src/Juggernaut/Cli.hs
@@ -1,12 +1,24 @@
 module Juggernaut.Cli where
 
 import Juggernaut.Api
-
-example :: Dependencies
-example = [
-    ("fred", []),
-    ("barney", ["fred"])
-  ]
+import System.Environment
+import System.Exit
 
 juggernaut :: IO ()
-juggernaut = putStrLn $ genmakefile example (\x -> "(cd " ++ x ++ " && ant)")
+juggernaut = getArgs >>= parse >>= putStr . (\x -> juggernaut' (read x))
+
+juggernaut' :: Dependencies -> String
+juggernaut' deps = genmakefile deps (\x -> "(cd " ++ x ++ " && ant)")
+
+parse :: [String] -> IO String
+parse (x:[]) = readFile x
+parse _ = usage >> die
+
+usage :: IO ()
+usage =  getProgName >>= putStrLn . (\x -> x ++ " dependency.file")
+  
+exit :: IO a
+exit = exitWith ExitSuccess
+
+die :: IO a
+die = exitWith $ ExitFailure 1
